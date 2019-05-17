90d4de0a1f53b3bde70dbf83858650f0f89fe0cf
jbwang <wangjiangbo@sudiyi.cn>
2019-05-17 10:21:13 +0800

    add flink use data source

---
+++ word-count/src/main/java/com/sudiyi/flink/model/Metric.java
@@ -0,0 +1,36 @@
+package com.sudiyi.flink.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * @author sdy
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Metric {
+
+    private String name;
+
+    private long timestamp;
+
+    private Map<String, Object> fields;
+
+    private Map<String, String> tags;
+
+    @Override
+    public String toString() {
+        return "Metric{" +
+                "name='" + name + '\'' +
+                ", timestamp=" + timestamp +
+                ", fields=" + fields +
+                ", tags=" + tags +
+                '}';
+    }
+
+
+}
